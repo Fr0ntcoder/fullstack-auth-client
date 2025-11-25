dcde458223e0feaa4b2ab97004abeef6bb08714b
Fr0ntcoder <creativ4lk@vk.com>
2025-11-25 08:26:35 +0300

    proxy/middleware

---
+++ src/app/dashboard/settings/page.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+	title: 'Админ панель/настройки'
+}
+
+export default function SettingsPage() {
+	return <div className=''>Dashboard</div>
+}
